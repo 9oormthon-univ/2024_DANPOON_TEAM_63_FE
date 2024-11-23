d6f097cadcb63f8daf206993540dda1eb3170d56
SEYOUNG PARK <seyeongb18@gmail.com>
2024-11-23 22:54:25 +0900

    [fix] bottom navigation 제거

--- app/src/main/java/com/example/youthspacefinder/presentation/MainActivity.kt
+++ app/src/main/java/com/example/youthspacefinder/presentation/MainActivity.kt
@@ -20,13 +20,13 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        val navController = navHostFragment.findNavController()
-        binding.bottomNavigationView.setupWithNavController(navController)
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+//        val navController = navHostFragment.findNavController()
+//        binding.bottomNavigationView.setupWithNavController(navController)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
